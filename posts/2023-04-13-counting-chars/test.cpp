d41f99d6932c9e72eca5f452034572d0dd5acc4d
Josiah Parry <josiah.parry@gmail.com>
2023-04-13 13:04:51 -0400

    change from grid to list

--- posts/2023-04-13-counting-chars/test.cpp
+++ posts/2023-04-13-counting-chars/test.cpp
@@ -1,28 +1,40 @@
-#include <Rcpp.h>
-using namespace Rcpp;
 
-#include <vector>
-#include <string>
-#include <algorithm>
+// //[[Rcpp::export]]
+// std::vector<size_t> count_seq_chars_to_ref_cpp(std::vector<std::string> x, const std::string& y) {
+//   std::vector<size_t> result;
+//   for (const auto& xi : x) {
+//     size_t count = 0;
+//     auto it_x = xi.begin();
+//     auto it_y = y.begin();
+//     while (it_x != xi.end() && it_y != y.end() && *it_x == *it_y) {
+//       ++count;
+//       ++it_x;
+//       ++it_y;
+//     }
+//     result.push_back(count);
+//   }
+//   return result;
+// }
 
-//[[Rcpp::export]]
-std::vector<size_t> count_seq_chars_to_ref_cpp(std::vector<std::string> x, const std::string& y) {
-  std::vector<size_t> result;
-  for (const auto& xi : x) {
-    size_t count = 0;
-    auto it_x = xi.begin();
-    auto it_y = y.begin();
-    while (it_x != xi.end() && it_y != y.end() && *it_x == *it_y) {
-      ++count;
-      ++it_x;
-      ++it_y;
+
+#include "cpp11.hpp"
+using namespace cpp11;
+
+[[cpp11::register]]
+integers count_seq_chars_to_ref(strings x, strings y) {
+  
+  int n = x.size();
+  writable::integers counts(n);
+  for (int i = 0; i < n; ++i) {
+    int count = 0;
+    for (size_t j = 0; j < x[i].size() && j < y.size(); ++j) {
+      if (x[i][j] == y[j]) {
+        ++count;
+      } else {
+        break;
+      }
     }
-    result.push_back(count);
+    counts[i] = count;
   }
-  return result;
+  return counts;
 }
-
-/*** R
-count_seq_chars_to_ref_cpp(c("abcd152", "abcd513", "abde324", "acde324", "abce532", "acde235"
-), "abcd123")
-*/
