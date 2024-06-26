1f875f99475987725e8450e7715c9d8556444328
Josiah Parry <josiah.parry@gmail.com>
2024-06-25 20:02:31 -0400

    make a prod api that reads csv files

---
+++ posts/2024-06-25-handle-files-plumber/plumber.R
@@ -0,0 +1,13 @@
+library(plumber)
+# R -e 'plumber::plumb("plumber.R")$run(port = 3000)'
+#* @post /upload
+upload <- function(req, res) {
+  mp <- mime::parse_multipart(req)
+  mp
+}
+
+#* @post /read_csv
+function(req, res) {
+  mp <- mime::parse_multipart(req)
+  readr::read_csv(mp$file$datapath)
+}
